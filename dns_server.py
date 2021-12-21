6f8c8a5a1c3328b7e81854c1a6fe1ad166d01a18
Pheliun <hollowedjelly@gmail.com>
2021-12-21 23:27:58 +0200

    added a lot of code

--- dns_server.py
+++ dns_server.py
@@ -1,28 +1,95 @@
 from scapy.layers.dns import *
 from scapy.sendrecv import sniff
-import os
-os.sys.path.append('/usr/local/lib/python2.7/site-packages')
-os.sys.path.append('/usr/bin')
+import socket
 
-def filter_dns(packet):
-    return (DNS in packet and packet[DNS].opcode == 0) #filters query dns packets
 
-print("sniffing...")
-packets = sniff(count=10,lfilter=filter_dns) #sniffing
-print("sniffing complete.")
+# filters the requirements needed for this DNS Server
+# input: packet
+# output: true or false
+def filter(packet):
+    return (IP in packet
+            # filters the following:
+            and packet[IP].dst == str(socket.gethostbyname(socket.gethostname()))  # same IP
+            and DNS in packet  # contains DNS
+            and DNSQR in packet  # contains DNSQR
+            and DNSRR not in packet  # contains DNSRR
+            and packet[DNS].opcode == 0  # it is a Query type
+            and UDP in packet  # supports UDP
+            and packet[UDP].dport == 53  # sent to port 53
+            and (packet[DNSQR].qtype == 1 or packet[DNSQR].qtype == 12))  # if it is type A(1) or PTR(12)
 
-for packet in packets:
-    packet.show()
 
-    # if not TCP in packet or UDP in packet and packet[TCP].dport == 53 or packet[UDP].dport ==53: #port 53
-    #     print("Packet does not meet the requirements")
-    #     break
+# checks if the IP sent is in the database
+# input: packet
+# output: true or false
+def exists(packet):
+    with open(r"C:\Users\Ben\PycharmProjects\Cyber\dns_server_records\dns_records.txt",
+              "r") as file:  # copies file into a variable
 
-    # if not packet[DNSQR].qtype == 1 and packet[DNSQR].qtype == 12: #type PTR or A
-    #     print("Packet does not contain the requirements")
-    #     break
+        # checks each line if the name exists in it
+        for line in file:
+            if packet[DNSQR].qname.decode() in line:
+                print("Found!!!")
+                return True
 
-    file = open(r"C:\Users\Ben\PycharmProjects\Cyber\dns_server_records\dns_records.txt", "r")
-    if packet[DNS].qname == file.read().split(' ')[0]:
-        print(file.read())
+            else:
+                print("NOT FOUND HAHAHAHA")
+        return False
 
+
+# sends the properties in the database by specifying it(for now only IP)
+# if no type was inputted then print all properties
+# input: packet, type of property
+# output: the property wanted
+def typeof_properties(packet, type=None):
+    with open(r"C:\Users\Ben\PycharmProjects\Cyber\dns_server_records\dns_records.txt", "r") as file:
+
+        if type.upper() == 'IP':  # sort of answer
+            # iterates through the file and returns the value wanted
+            for line in file:
+                try:
+                    if packet[DNSQR].qname.decode() in line:
+                        return line.split(';')[1]
+                except:
+                    continue
+
+        return None  # exeptional case (I dont even know if that case is possible)
+
+
+def send_packet(packet):
+
+    #the following changes are necessary in order to send a valid packet back to the client:
+    ans_packet = IP(dst=packet[IP].src,
+                    src=str(socket.gethostbyname(socket.gethostname())) /
+                        UDP(dport=packet[IP].sport, sport=53) /
+                        DNS(id=packet[DNS].id, qr=1, qd=packet[DNSQR], an=
+                        DNSRR(rrname=packet[DNSQR].qname.decode(),
+                              type=packet[DNSQR].qtype.decode())))
+
+    if exists(packet):
+
+        if packet[DNSQR].qtype == 1:
+            ans_packet = DNS(rdata = typeof_properties(packet, 'ip'))
+
+        elif packet[DNSQR].qtype == 12:
+            ans_packet = DNS(rdata = "Ben-PC DNS Server")
+
+
+    else:
+
+
+
+
+
+def main():
+    print("sniffing...")
+    packet = sniff(count=1, lfilter=filter)[0]  # sniffing
+    print("sniffing complete.")
+    send_packet(packet)
+
+
+if __name__ == '__main__':
+    main()
+
+    # except ValueError and AttributeError and IndexError:
+    #     print("The packet does not contain the attribute or value needed")
